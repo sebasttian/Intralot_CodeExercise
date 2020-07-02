f1fc262a187c161c282ed6461e34036dd54e8b98
sebasttian <sebasttiansobenes@gmail.com>
2020-07-02 12:40:12 -0400

    Added Internet permission in manifest as well as constants for connectivity to Yelp API.

---
+++ Intralot_Coding_Exercise/app/src/main/java/com/ss/intralot_coding_exercise/Constants.java
@@ -0,0 +1,12 @@
+package com.ss.intralot_coding_exercise;
+
+/**
+ * Sebasttian Sobenes
+ * Constants.java
+ */
+
+public class Constants {
+    public static final String YELP_TOKEN = BuildConfig.YELP_TOKEN;
+    public static final String YELP_BASE_URL = "https://api.yelp.com/v3/businesses/search?term=physical therapist";
+    public static final String YELP_LOCATION_QUERY_PARAMETER = "location";
+}
